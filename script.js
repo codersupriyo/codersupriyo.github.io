48d8be330d57ef07f45ad56eeb5cd188349a3695
unknown <supriyo.developer@gmail.com>
2020-09-13 21:22:58 +0530

    Site test

---
+++ script.js
@@ -0,0 +1,35 @@
+function mode() {
+    // Mode change
+  
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+  
+    let navBar = document.getElementById("myTopnav");
+    navBar.classList.toggle("dark-nav");
+  
+    let mainContent = document.getElementById("main-content");
+    mainContent.classList.toggle("dark-nav");
+  
+    // Changing text of the button
+  
+    let change = document.getElementById("modeBTN");
+  
+    if (change.innerHTML == "Light mode ☀️") {
+      change.innerHTML = "Dark mode 🌑";
+    } else {
+      change.innerHTML = "Light mode ☀️";
+    }
+  }
+  
+  // Responsive navbar
+  
+  function navBar() {
+    let bar = document.getElementById("myTopnav");
+  
+    if (bar.className === "topnav") {
+      bar.className += " responsive";
+    } else {
+      bar.className = "topnav";
+    }
+  }
+  
